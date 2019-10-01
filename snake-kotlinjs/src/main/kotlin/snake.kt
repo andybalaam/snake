60ab2f165ce2905a474bf09e4913a4761c344591
Andy Balaam <andybalaam@artificialworlds.net>
2019-10-01 10:47:17 +0100

    Print canvas height

--- snake-kotlinjs/src/main/kotlin/snake.kt
+++ snake-kotlinjs/src/main/kotlin/snake.kt
@@ -1,6 +1,14 @@
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    println("foo")
-    println(document.getElementById("snake-canvas"))
+    val canvas = document.getElementById("snake-canvas") as HTMLCanvasElement;
+
+    println("height=${canvas.height}")
+    println("width=${canvas.width}")
+    val ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+
+    ctx.fillStyle = "#F00"
+    ctx.fillRect(10.0, 10.0, 20.0, 20.0)
 }
